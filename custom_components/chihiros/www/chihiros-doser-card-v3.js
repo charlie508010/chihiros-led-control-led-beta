70f7f15c29a37a725ac7c2cf13043310d8be5f7d
charlie508010 <285831950+charlie508010@users.noreply.github.com>
2026-07-19 07:51:21 +0200

    chore: remove accidental script log files

--- custom_components/chihiros/www/chihiros-doser-card-v3.js
+++ custom_components/chihiros/www/chihiros-doser-card-v3.js
@@ -1,5 +1,5 @@
 import "./chihiros-notification-ui.js?v=0.1.0";
-import "./panels/chihiros-led-panel.js?v=0.2.1005";
+import "./panels/chihiros-led-panel.js?v=0.2.1006";
 
 class ChihirosDoserCard extends window.ChihirosLedPanelMixin(HTMLElement) {
   setConfig(config) {
