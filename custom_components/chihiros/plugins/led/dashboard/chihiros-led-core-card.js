c7a2ba0d87164978109eea0c82a1837b44a0b4e0
charlie508010 <charlie508010@users.noreply.github.com>
2026-07-20 22:58:33 +0200

    docs: clean Wireshark scheduler debug sample format

--- custom_components/chihiros/plugins/led/dashboard/chihiros-led-core-card.js
+++ custom_components/chihiros/plugins/led/dashboard/chihiros-led-core-card.js
@@ -1,5 +1,5 @@
 import "./chihiros-notification-ui.js?v=0.1.1";
-import "./panels/chihiros-led-panel.js?v=0.2.1069";
+import "./panels/chihiros-led-panel.js?v=0.2.1070";
 
 class ChihirosLedCoreCard extends window.ChihirosLedPanelMixin(HTMLElement) {
   setConfig(config) {
