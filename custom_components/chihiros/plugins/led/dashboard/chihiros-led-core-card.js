8c9c92211a0a7e1b3e7ffec9f6dfdd8e20eb97eb
charlie508010 <charlie508010@users.noreply.github.com>
2026-07-20 19:19:52 +0200

    chore: remove Wireshark package from LED beta

--- custom_components/chihiros/plugins/led/dashboard/chihiros-led-core-card.js
+++ custom_components/chihiros/plugins/led/dashboard/chihiros-led-core-card.js
@@ -1,5 +1,5 @@
 import "./chihiros-notification-ui.js?v=0.1.1";
-import "./panels/chihiros-led-panel.js?v=0.2.1063";
+import "./panels/chihiros-led-panel.js?v=0.2.1064";
 
 class ChihirosLedCoreCard extends window.ChihirosLedPanelMixin(HTMLElement) {
   setConfig(config) {
