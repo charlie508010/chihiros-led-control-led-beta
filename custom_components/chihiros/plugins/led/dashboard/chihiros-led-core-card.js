0e14f3e29a11c0b29e00e8fb1e3f24314eb3b07b
charlie508010 <285831950+charlie508010@users.noreply.github.com>
2026-07-21 14:35:52 +0200

    ci: update GitHub checkout action to Node 24

--- custom_components/chihiros/plugins/led/dashboard/chihiros-led-core-card.js
+++ custom_components/chihiros/plugins/led/dashboard/chihiros-led-core-card.js
@@ -1,5 +1,5 @@
 import "./chihiros-notification-ui.js?v=0.1.1";
-import "./panels/chihiros-led-panel.js?v=0.2.1157";
+import "./panels/chihiros-led-panel.js?v=0.2.1158";
 
 class ChihirosLedCoreCard extends window.ChihirosLedPanelMixin(HTMLElement) {
   setConfig(config) {
