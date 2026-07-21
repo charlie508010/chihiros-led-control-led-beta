1ca4621136415f28b2fa85155fa29f61a9a53d71
charlie508010 <charlie508010@users.noreply.github.com>
2026-07-21 14:15:52 +0200

    chore: rewrite commit authors to charlie508010

--- custom_components/chihiros/plugins/led/dashboard/chihiros-led-core-card.js
+++ custom_components/chihiros/plugins/led/dashboard/chihiros-led-core-card.js
@@ -1,5 +1,5 @@
 import "./chihiros-notification-ui.js?v=0.1.1";
-import "./panels/chihiros-led-panel.js?v=0.2.1155";
+import "./panels/chihiros-led-panel.js?v=0.2.1156";
 
 class ChihirosLedCoreCard extends window.ChihirosLedPanelMixin(HTMLElement) {
   setConfig(config) {
