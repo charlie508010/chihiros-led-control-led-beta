d3f850a7b0539bb7e590f42e008b2ed17ac569c7
charlie508010 <285831950+charlie508010@users.noreply.github.com>
2026-07-21 15:16:46 +0200

    ci: remove automatic release workflow

--- custom_components/chihiros/plugins/led/dashboard/chihiros-led-core-card.js
+++ custom_components/chihiros/plugins/led/dashboard/chihiros-led-core-card.js
@@ -1,5 +1,5 @@
 import "./chihiros-notification-ui.js?v=0.1.1";
-import "./panels/chihiros-led-panel.js?v=0.2.1162";
+import "./panels/chihiros-led-panel.js?v=0.2.1163";
 
 class ChihirosLedCoreCard extends window.ChihirosLedPanelMixin(HTMLElement) {
   setConfig(config) {
