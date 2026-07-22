2697f7bfb06fdb2f621dbd2408c8f504b3147711
charlie508010 <285831950+charlie508010@users.noreply.github.com>
2026-07-22 09:16:04 +0200

    fix: include schedule range in LED debug flow blocks

--- custom_components/chihiros/plugins/led/dashboard/chihiros-led-core-card.js
+++ custom_components/chihiros/plugins/led/dashboard/chihiros-led-core-card.js
@@ -1,5 +1,5 @@
 import "./chihiros-notification-ui.js?v=0.1.1";
-import "./panels/chihiros-led-panel.js?v=0.2.1192";
+import "./panels/chihiros-led-panel.js?v=0.2.1193";
 
 class ChihirosLedCoreCard extends window.ChihirosLedPanelMixin(HTMLElement) {
   setConfig(config) {
