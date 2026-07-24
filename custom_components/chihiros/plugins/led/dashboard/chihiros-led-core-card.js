bb8498fad87ed07c7a6ed871fcdebbb5c6763ba7
charlie508010 <285831950+charlie508010@users.noreply.github.com>
2026-07-24 10:31:31 +0200

    fix: restore registry database from backup

--- custom_components/chihiros/plugins/led/dashboard/chihiros-led-core-card.js
+++ custom_components/chihiros/plugins/led/dashboard/chihiros-led-core-card.js
@@ -1,5 +1,5 @@
 import "./chihiros-notification-ui.js?v=0.1.1";
-import "./panels/chihiros-led-panel.js?v=0.2.1244";
+import "./panels/chihiros-led-panel.js?v=0.2.1245";
 
 class ChihirosLedCoreCard extends window.ChihirosLedPanelMixin(HTMLElement) {
   setConfig(config) {
